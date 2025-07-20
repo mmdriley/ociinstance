52e8d51800375025c604af2cebd87d3384b8747e
Matthew Riley <mdriley@gmail.com>
2025-07-20 01:54:28 -0700

    Ran `tofu fmt`

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 variable "oci_specifics" {
-    type = object({
-        region = string  # e.g. "us-sanjose-1"
+  type = object({
+    region = string # e.g. "us-sanjose-1"
 
-        # https://docs.oracle.com/en-us/iaas/Content/API/Concepts/apisigningkey.htm#five
-        tenancy_ocid = string
-        user_ocid = string
+    # https://docs.oracle.com/en-us/iaas/Content/API/Concepts/apisigningkey.htm#five
+    tenancy_ocid = string
+    user_ocid    = string
 
-        # openssl rsa -in ~/.oci/apikey.pem -pubout -outform DER | openssl md5 -c
-        # ref: https://docs.oracle.com/en-us/iaas/Content/API/Concepts/apisigningkey.htm#four
-        public_key_fingerprint = string
+    # openssl rsa -in ~/.oci/apikey.pem -pubout -outform DER | openssl md5 -c
+    # ref: https://docs.oracle.com/en-us/iaas/Content/API/Concepts/apisigningkey.htm#four
+    public_key_fingerprint = string
 
-        private_key_path = optional(string, "~/.oci/apikey.pem")
-    })
+    private_key_path = optional(string, "~/.oci/apikey.pem")
+  })
 
-    validation {
-        condition = fileexists(var.oci_specifics.private_key_path)
-        error_message = "private_key_path references a file that does not exist"
-    }
+  validation {
+    condition     = fileexists(var.oci_specifics.private_key_path)
+    error_message = "private_key_path references a file that does not exist"
+  }
 }
 
 variable "ssh_authorized_keys" {
@@ -25,12 +25,12 @@ variable "ssh_authorized_keys" {
 
 provider "oci" {
   tenancy_ocid = var.oci_specifics.tenancy_ocid
-  user_ocid = var.oci_specifics.user_ocid
+  user_ocid    = var.oci_specifics.user_ocid
 
   region = var.oci_specifics.region
 
   private_key_path = var.oci_specifics.private_key_path
-  fingerprint = var.oci_specifics.public_key_fingerprint
+  fingerprint      = var.oci_specifics.public_key_fingerprint
 }
 
 locals {
@@ -51,7 +51,7 @@ data "oci_core_images" "ubuntu_arm64_images" {
 
   state = "AVAILABLE"
 
-  sort_by = "DISPLAYNAME"
+  sort_by    = "DISPLAYNAME"
   sort_order = "DESC"
 
   filter {
@@ -86,11 +86,11 @@ resource "oci_core_vcn" "the_network" {
 
 resource "oci_core_security_list" "the_security_list" {
   compartment_id = local.oci_compartment_id
-  vcn_id = oci_core_vcn.the_network.id
+  vcn_id         = oci_core_vcn.the_network.id
 
   ingress_security_rules {
-    protocol = "6"  # TCP
-    source = "0.0.0.0/0"
+    protocol = "6" # TCP
+    source   = "0.0.0.0/0"
     tcp_options {
       min = 22
       max = 22
@@ -98,7 +98,7 @@ resource "oci_core_security_list" "the_security_list" {
   }
 
   egress_security_rules {
-    protocol = "6"  # TCP
+    protocol    = "6" # TCP
     destination = "0.0.0.0/0"
   }
 
@@ -107,16 +107,16 @@ resource "oci_core_security_list" "the_security_list" {
 
 resource "oci_core_internet_gateway" "the_network_gateway" {
   compartment_id = local.oci_compartment_id
-  vcn_id = oci_core_vcn.the_network.id
+  vcn_id         = oci_core_vcn.the_network.id
 }
 
 resource "oci_core_route_table" "the_routing_table" {
   compartment_id = local.oci_compartment_id
-  vcn_id = oci_core_vcn.the_network.id
+  vcn_id         = oci_core_vcn.the_network.id
 
   route_rules {
     network_entity_id = oci_core_internet_gateway.the_network_gateway.id
-    destination = "0.0.0.0/0"
+    destination       = "0.0.0.0/0"
   }
 
   # TODO: IPv6
@@ -124,9 +124,9 @@ resource "oci_core_route_table" "the_routing_table" {
 
 resource "oci_core_subnet" "the_subnet" {
   compartment_id = local.oci_compartment_id
-  vcn_id = oci_core_vcn.the_network.id
+  vcn_id         = oci_core_vcn.the_network.id
 
-  cidr_block = cidrsubnet(oci_core_vcn.the_network.cidr_blocks[0], 8, local.arbitrary_ipv4_octet)
+  cidr_block     = cidrsubnet(oci_core_vcn.the_network.cidr_blocks[0], 8, local.arbitrary_ipv4_octet)
   ipv6cidr_block = cidrsubnet(oci_core_vcn.the_network.ipv6cidr_blocks[0], 8, local.arbitrary_ipv6_octet)
 
   security_list_ids = [
@@ -135,12 +135,12 @@ resource "oci_core_subnet" "the_subnet" {
 }
 
 resource "oci_core_route_table_attachment" "route_table_to_subnet" {
-  subnet_id = oci_core_subnet.the_subnet.id
+  subnet_id      = oci_core_subnet.the_subnet.id
   route_table_id = oci_core_route_table.the_routing_table.id
 }
 
 resource "oci_core_instance" "the_instance" {
-  compartment_id = local.oci_compartment_id
+  compartment_id      = local.oci_compartment_id
   availability_domain = data.oci_identity_availability_domains.all_domains.availability_domains[0].name
 
   instance_options {
@@ -150,22 +150,22 @@ resource "oci_core_instance" "the_instance" {
   shape = "VM.Standard.A1.Flex"
   shape_config {
     memory_in_gbs = 6
-    ocpus = 1
+    ocpus         = 1
   }
 
   create_vnic_details {
     assign_ipv6ip = true
-    subnet_id = oci_core_subnet.the_subnet.id
+    subnet_id     = oci_core_subnet.the_subnet.id
   }
 
   source_details {
     source_type = "image"
-    source_id = data.oci_core_images.ubuntu_arm64_images.images[0].id
+    source_id   = data.oci_core_images.ubuntu_arm64_images.images[0].id
 
     boot_volume_size_in_gbs = 90
   }
 
   metadata = {
-    "ssh_authorized_keys": join("\n", var.ssh_authorized_keys)
+    "ssh_authorized_keys" : join("\n", var.ssh_authorized_keys)
   }
 }
