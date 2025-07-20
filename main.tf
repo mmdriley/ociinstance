017decb501401333797a804afc92770384a5c915
Matthew Riley <mdriley@gmail.com>
2025-07-20 01:52:45 -0700

    Checkpoint: this was enough to create an instance I could SSH to

--- main.tf
+++ main.tf
@@ -19,6 +19,10 @@ variable "oci_specifics" {
     }
 }
 
+variable "ssh_authorized_keys" {
+  type = list(string)
+}
+
 provider "oci" {
   tenancy_ocid = var.oci_specifics.tenancy_ocid
   user_ocid = var.oci_specifics.user_ocid
@@ -38,7 +42,7 @@ locals {
   arbitrary_ipv6_octet = parseint("45", 16)
 }
 
-data "oci_identity_availability_domains" "domains" {
+data "oci_identity_availability_domains" "all_domains" {
   compartment_id = local.oci_compartment_id
 }
 
@@ -80,24 +84,88 @@ resource "oci_core_vcn" "the_network" {
   is_ipv6enabled = true
 }
 
+resource "oci_core_security_list" "the_security_list" {
+  compartment_id = local.oci_compartment_id
+  vcn_id = oci_core_vcn.the_network.id
+
+  ingress_security_rules {
+    protocol = "6"  # TCP
+    source = "0.0.0.0/0"
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }
+
+  egress_security_rules {
+    protocol = "6"  # TCP
+    destination = "0.0.0.0/0"
+  }
+
+  # TODO: IPv6, UDP, ICMP
+}
+
+resource "oci_core_internet_gateway" "the_network_gateway" {
+  compartment_id = local.oci_compartment_id
+  vcn_id = oci_core_vcn.the_network.id
+}
+
+resource "oci_core_route_table" "the_routing_table" {
+  compartment_id = local.oci_compartment_id
+  vcn_id = oci_core_vcn.the_network.id
+
+  route_rules {
+    network_entity_id = oci_core_internet_gateway.the_network_gateway.id
+    destination = "0.0.0.0/0"
+  }
+
+  # TODO: IPv6
+}
+
 resource "oci_core_subnet" "the_subnet" {
   compartment_id = local.oci_compartment_id
   vcn_id = oci_core_vcn.the_network.id
 
   cidr_block = cidrsubnet(oci_core_vcn.the_network.cidr_blocks[0], 8, local.arbitrary_ipv4_octet)
   ipv6cidr_block = cidrsubnet(oci_core_vcn.the_network.ipv6cidr_blocks[0], 8, local.arbitrary_ipv6_octet)
+
+  security_list_ids = [
+    oci_core_security_list.the_security_list.id,
+  ]
 }
 
-output "things_i_know_oci_core_instance_will_need" {
-  value = {
-    image = data.oci_core_images.ubuntu_arm64_images.images[0].id
-    domain = data.oci_identity_availability_domains.domains.availability_domains[0].name
-  }
+resource "oci_core_route_table_attachment" "route_table_to_subnet" {
+  subnet_id = oci_core_subnet.the_subnet.id
+  route_table_id = oci_core_route_table.the_routing_table.id
 }
 
-/*
 resource "oci_core_instance" "the_instance" {
   compartment_id = local.oci_compartment_id
+  availability_domain = data.oci_identity_availability_domains.all_domains.availability_domains[0].name
+
+  instance_options {
+    are_legacy_imds_endpoints_disabled = true
+  }
+
   shape = "VM.Standard.A1.Flex"
+  shape_config {
+    memory_in_gbs = 6
+    ocpus = 1
+  }
+
+  create_vnic_details {
+    assign_ipv6ip = true
+    subnet_id = oci_core_subnet.the_subnet.id
+  }
+
+  source_details {
+    source_type = "image"
+    source_id = data.oci_core_images.ubuntu_arm64_images.images[0].id
+
+    boot_volume_size_in_gbs = 90
+  }
+
+  metadata = {
+    "ssh_authorized_keys": join("\n", var.ssh_authorized_keys)
+  }
 }
-*/
