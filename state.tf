0e42496a974a5502c6e1592891e3e868c2ac1915
Matthew Riley <mdriley@gmail.com>
2025-07-20 17:31:55 -0700

    Encrypt state and check in
    Remove the code to support migration.

--- state.tf
+++ state.tf
@@ -6,18 +6,13 @@ terraform {
       passphrase = var.state_encryption_passphrase
     }
 
-    method "unencrypted" "for_migration" {}
-
     method "aes_gcm" "encrypted" {
       keys = key_provider.pbkdf2.passphrase_key
     }
 
     state {
       method = method.aes_gcm.encrypted
-
-      fallback {
-        method = method.unencrypted.for_migration
-      }
+      enforced = true
     }
 
     # not bothering to encrypt plan
