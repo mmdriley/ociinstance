4c4cc08683b135a8de758a4e74c3d2bb2ff8bdb5
Matthew Riley <mdriley@gmail.com>
2025-07-22 22:26:58 -0700

    Move state into subdir based on OCI tenancy
    In case someone forks the repo, they won't start off with a state they can't decrypt.

--- state.tf
+++ state.tf
@@ -1,5 +1,9 @@
 terraform {
-  backend "local" {}
+  backend "local" {
+    # Use different state files for different OCI tenancies, to avoid
+    # conflicts if someone forks this repo.
+    path = "states/${sha256(var.oci_specifics.tenancy_ocid)}/terraform.tfstate"
+  }
 
   encryption {
     key_provider "pbkdf2" "passphrase_key" {
